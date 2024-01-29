cae281103473610f84f4c71b507e818c3494ca90
Alejandro Elustondo <aleelus@hotmail.com>
2024-01-29 11:36:18 -0300

    event transfer test

---
+++ src/mapping.ts
@@ -0,0 +1,11 @@
+import { Transfer as TransferEvent } from "../generated/Contract/Contract"
+import { Transfer } from "../generated/schema"
+
+export function handleTransfer(event: TransferEvent): void {
+  let entity = new Transfer(
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  )
+  entity.from = event.params.from
+  entity.to = event.params.to
+  entity.save()
+}
